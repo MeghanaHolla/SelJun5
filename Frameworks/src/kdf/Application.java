f7f1781d2159ebc8d33c40d8fcb326532c31448b
MEGHANA <meghanaholla8526@gmail.com>
2022-06-05 21:27:29 +0530

    making changes to script

--- Frameworks/src/kdf/Application.java
+++ Frameworks/src/kdf/Application.java
@@ -13,6 +13,7 @@ public class Application {
 	public void verifyInvalidLogin() throws IOException {
 		String[][] data = GenericMethods.getData("D:\\Sel7May\\TestData.xlsx", "Sheet2");
 		Methods mtd = new Methods();
+		//Making changes to application class
 		for(int i = 1; i<data.length;i++) {
 			switch(data[i][3]) {
 			
